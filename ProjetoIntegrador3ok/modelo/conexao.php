f042fc66c3ecaa21b9acd3c596e5736d166eab1e
cbrunogoncalves@github.com <buno@localhost>
2018-04-07 18:04:02 -0300

    Alteração do codigo na conexao com banco de dados

--- ProjetoIntegrador3ok/modelo/conexao.php
+++ ProjetoIntegrador3ok/modelo/conexao.php
@@ -1,16 +1,13 @@
 <?php
 //as variaveis digitadarecebe os dados digitado na pagina aonterior 
 
-
-
 //conexão mysql
 //Configura de acesso ao banco de dados
-$conf['host'] = "localhost"; //host do banco de dados
-$conf['bd'] = "bancodedados"; //nome do banco
-$conf['usuario'] = "root"; //nome de usuário de acesso ao banco
-$conf['senha'] = ""; //senha de acesso ao banco de dados
+$conexao = new mysqli_connect("localhost", "root", "root", "bancodedados");
 
-//conexão com o banco de dados
-$conexao = @mysql_connect($conf['host'], $conf['usuario'], $conf['senha']) or die("ERRO NA CONEXÃO COM MYSQL");
-$db = @mysql_select_db($conf['bd'], $conexao) or die("ERRO NA SELEÇÃO DO BANCO DE DADOS MYSQL");
+if(mysqli_connect_errno()){
+    echo "Failed connect to mysq:". mysqli_connect_error();
+    }
+    
+    return $conexao;
 ?>
