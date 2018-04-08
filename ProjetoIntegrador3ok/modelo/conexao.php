8cc7bf8f9a161e06e991cad193bf5809cca9a9de
cbrunogoncalves@github.com <buno@localhost>
2018-04-08 16:18:53 -0300

    Alteração do código - Conexão.php

--- ProjetoIntegrador3ok/modelo/conexao.php
+++ ProjetoIntegrador3ok/modelo/conexao.php
@@ -1,13 +1,11 @@
 <?php
-//as variaveis digitadarecebe os dados digitado na pagina aonterior 
 
-//conexão mysql
 //Configura de acesso ao banco de dados
 $conexao = new mysqli_connect("localhost", "root", "root", "bancodedados");
 
-if(mysqli_connect_errno()){
-    echo "Failed connect to mysq:". mysqli_connect_error();
-    }
-    
-    return $conexao;
+if (mysqli_connect_errno()) {
+    echo "Failed connect to mysq:" . mysqli_connect_error();
+}
+
+return $conexao;
 ?>
