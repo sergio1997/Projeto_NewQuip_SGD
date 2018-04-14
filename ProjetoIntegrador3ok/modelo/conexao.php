ed83d22c071787555e0c1e72b6549a1402970381
Erlanio Freire <Erlanio Freire@DESKTOP-99MVAUV>
2018-04-14 10:17:31 -0300

    Correção de erro do index.php

--- ProjetoIntegrador3ok/modelo/conexao.php
+++ ProjetoIntegrador3ok/modelo/conexao.php
@@ -2,7 +2,7 @@
 
 //Configura de acesso ao banco de dados
 
-$conexao = new mysqli_connect("localhost", "root", "", "bancodedados");
+$conexao = new mysqli_connect("localhost", "root", "root", "bancodedados");
 if (mysqli_connect_errno()) {
     echo "Failed connect to mysq:" . mysqli_connect_error();
 }
