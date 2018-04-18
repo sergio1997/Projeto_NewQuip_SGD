baca2de3ed44709db4fb3c16e501f5ddbf170a01
sergio1997 <sergiolavor123321@gmail.com>
2018-04-17 21:48:43 -0300

    Correção do erro no arquivo login.php

---
+++ modelo/login.php
@@ -0,0 +1,91 @@
+
+<html>
+<head>
+<title></title>
+
+</head>
+
+<script type= "text/javascript">
+//função para redirecionar para outra pagina apos 5 segudos 
+//caso aultentique ou não 
+function loginsuccessfully(){
+	
+	setTimeout("window.location='../visao/pagina_inicial.php'");
+	
+	
+}
+function loginfailed(){
+	
+	setTimeout("window.location='../visao/index.php'");
+	
+	alert('email ou senha invalida');
+}
+</script>
+
+
+<body class="hold-transition skin-blue layout-top-nav">
+	
+
+		<?PHP
+
+		include "../modelo/conexao.php";
+?>
+
+
+<?php
+$login = $_POST['email'];
+$senha = $_POST['senha'];
+
+//Comando SQL de verificação de autenticação
+
+$sql1 = "SELECT *
+FROM funcionario
+WHERE email = '$login'
+AND senha = '$senha'";
+
+
+$resultado1 = mysqli_query($conexao,$sql1) or die ("Erro na seleção da tabela.");
+ 
+//Caso consiga logar cria a sessão
+
+ 
+	if (mysqli_num_rows($resultado1)>0) {
+  # code...
+
+  @session_start();
+
+
+       
+    $_SESSION['email'] = $login;
+    $_SESSION['senha'] = $senha;
+    
+echo "$login";
+echo"$senha";
+     echo "<script>loginsuccessfully()</script>";
+  # code...
+}
+
+ 
+//Caso contrário redireciona para a página de autenticação
+else {
+	
+    //Destrói
+    @session_destroy();
+ 
+ 
+    //Limpa
+    
+ 
+    //Redireciona para a página de autenticação
+	echo "<script>loginfailed()</script>";
+    
+     
+}
+?>
+   
+        
+
+
+</body>
+
+</html>
