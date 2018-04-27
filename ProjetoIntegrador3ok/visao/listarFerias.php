7f6677a3c2c122aa447472d36b4ee598dd415680
cbrunogoncalves@github.com <buno@localhost>
2018-04-26 23:36:24 -0300

    Nova Funcionalidade Listar ferias, Visão.

--- ProjetoIntegrador3ok/visao/listarFerias.php
+++ ProjetoIntegrador3ok/visao/listarFerias.php
@@ -1,15 +1,86 @@
 <?php
-
 // esse bloco de código em php verifica se existe a sessão, pois o usuário pode simplesmente não fazer o login e digitar na barra de endereço do seu navegador o caminho para a página principal do site (sistema), burlando assim a obrigação de fazer um login, com isso se ele não estiver feito o login não será criado a session, então ao verificar que a session não existe a página redireciona o mesmo para a index.php. 
- session_start();
+session_start();
 
- if((!isset ($_SESSION['email']) == true) and (!isset ($_SESSION['senha']) == true)) { 
- unset($_SESSION['email']);
- unset($_SESSION['senha']); 
- header('location:index.php');
-  
- }
- //grava a sessão dos dados do usario logando atarvs de uma sessão 
- $logado = $_SESSION['email'];
-	
- ?>
+if ((!isset($_SESSION['email']) == true) and ( !isset($_SESSION['senha']) == true)) {
+    unset($_SESSION['email']);
+    unset($_SESSION['senha']);
+    header('location:index.php');
+}
+//grava a sessão dos dados do usario logando atarvs de uma sessão 
+$logado = $_SESSION['email'];
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>SGD</title>
+        <!-- Parte responsiva -->
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <!-- Bootstrap 3.3.5 -->
+        <link rel="stylesheet" href="../visao/bootstrap/css/bootstrap.min.css">
+        <!-- Fontes usadas -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <!-- icones -->
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <!-- tema -->
+        <link rel="stylesheet" href="../visao/dist/css/AdminLTE.min.css">
+
+        <link rel="stylesheet" href="../visao/dist/css/skins/skin-blue.min.css">
+        <script language="Javascript">
+            //alerta se deseja excluir ou não os dados
+            function confirmacao() {
+                var resposta = confirm("Tem certeza que desejaremover seus dados?");
+
+                if (resposta == true) {
+                    window.location.href = "../modelo/ExcluirConta.php?nome=$exibe[nome]";
+                }
+            }
+        </script>
+        <script language="Javascript">
+            //alerta se deseja excluir ou não os dados
+            function confirmacaoEx() {
+                var resposta = confirm("Tem certeza que desejaremover seus dados?");
+
+                if (resposta == true) {
+                    window.location = "../visao/Alterarfuncionario.php?id=<?php echo $linha['id']; ?>";
+                }
+            }
+        </script>
+
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+
+            <header class="main-header">
+
+                <a href="index2.html" class="logo">
+
+                    <span class="logo-mini"><b>S</b>GD</span>
+                    <span class="logo-lg"><b>SGD</b></span>
+                </a>
+
+                <nav class="navbar navbar-static-top" role="navigation">
+                    <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                        <span class="sr-only">Toggle navigation</span>
+                    </a>
+                    <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="pagina_inicial.php">Inicio</a></li>
+                            <li data-toggle="modal" data-target="#suporte"><a href="#">Suporte</a></li>
+                            <li data-toggle="modal" data-target="#contato"><a href="#">Ajuda</a></li>
+                        </ul>
+                    </div>
+                    <div class="navbar-custom-menu">
+                        <ul class="nav navbar-nav">
+                            <li><a><?php echo $logado ?></a></li>
+                            <li><a  href="../controle/logout.php" >» Logout</a></li>
+                            <li class="dropdown user user-menu"></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header
+
+    </body>
+</html>
