ea3eef8bbc6d2932572f5e0872cff136f12df730
sergio1997 <sergiolavor123321@gmail.com>
2018-04-27 00:51:43 -0300

    nova funcionalodade desligamento visao

--- ProjetoIntegrador3ok/visao/desligamento.php
+++ ProjetoIntegrador3ok/visao/desligamento.php
@@ -74,3 +74,81 @@ function valida_dados (nomeform)
   alert("dados salvos com sucesso!!");
   return true;
 }
+</script>
+<script language="javascript1.2">
+      function soLetras(e){
+      if (document.all){var evt=event.keyCode;}
+      else{var evt = e.charCode;}
+      var chr = String.fromCharCode(evt);
+      //  var re = /[A-Za-z]/; // permite apenas de A-Z e de a-z
+      var re = /[A-Za-z\s-ÃÕÑÁÉÍÓÚÀÜÇãõñáéíóúàçü]/; // permite de A-Z, a-z, espaços,
+      // hífens e caracteres acentuados. Mais caracteres podem ser adicionados a Lista
+      return (re.test(chr) || evt<20); // com evt<20 permitimos <ENTER>,<TAB>,<BACKSPACE>
+      }
+</script>
+<script language="javascript1.2">
+      function soNumero(e){
+      if (document.all){var evt=event.keyCode;}
+      else{var evt = e.charCode;}
+      var chr = String.fromCharCode(evt);
+      //  var re = /[0-9/; // permite apenas numeros de 0-9;
+      var re = /[0-9]/; // permite numeros  de 0-9,
+      // hífens e caracteres acentuados. Mais caracteres podem ser adicionados a Lista
+      return (re.test(chr) || evt<20); 
+      }
+</script>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SGD</title>
+  <!-- Parte responsiva -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.5 -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <!-- Fontes usadas -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- icones -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- tema -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+
+  <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
+  
+ <script language="Javascript">
+  //alerta se deseja excluir ou não os dados
+function confirmacao() {
+     var resposta = confirm("Tem certeza que desejaremover seus dados?");
+ 
+     if (resposta == true) {
+          window.location.href="../modelo/ExcluirConta.php?nome=$exibe[nome]";
+     }
+}
+</script>
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+  <header class="main-header">
+      <a href="index2.html" class="logo">
+      <span class="logo-mini"><b>S</b>GD</span>
+      <span class="logo-lg"><b>SGD</b></span>
+    </a>
+    <nav class="navbar navbar-static-top" role="navigation">
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+         <ul class="nav navbar-nav">
+            <li><a href="pagina_inicial.php">Inicio</a></li>
+            <li data-toggle="modal" data-target="#suporte"><a href="#">Suporte</a></li>
+            <li data-toggle="modal" data-target="#contato"><a href="#">Ajuda</a></li>
+          </ul>
+        </div>
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+            <li><a><?php echo $logado ?></a></li>
+            <li><a  href="../controle/logout.php" >» Logout</a></li>
+            <li class="dropdown user user-menu"></li>
+          </ul>
+        </div>
+    </nav>
+  </header>
+</header>
