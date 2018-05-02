a570e96ea5e4c2cf74f2f745f1bc3385f78ff346
sergio1997 <sergiolavor123321@gmail.com>
2018-05-02 18:03:18 -0300

    nova funcionalidade, inserir desligamento

---
+++ ProjetoIntegrador3ok/modelo/insereDesligamento.php
@@ -0,0 +1,41 @@
+<?php 
+
+include "../modelo/conexao.php";
+
+$nomeFuncionario=$_POST['funcionario'];
+
+
+$questao1=$_POST['funcionario'];
+$dataSaida=$_POST['dataSaida'];
+$MotivoDesligamento=$_POST['MotivoDesligamento'];
+
+//questão 1
+$questao1=$_POST['q1'];
+$questao1PQ=$_POST['q1PQ'];
+
+//questão 2
+$questao2=$_POST['q2'];
+
+//questão 3
+$questao3=$_POST['q3'];
+
+//questão 4
+$questao4=$_POST['q4'];
+
+$q4Quantidade=$_POST['q4Quantidade'];
+
+//questão 5
+$questao51=$_POST['q51'];
+$questao52=$_POST['q52'];
+$questao53=$_POST['q53'];
+$questao54=$_POST['q54'];
+$questao55=$_POST['q55'];
+
+$q5observacao=$_POST['q5observacao'];
+
+//questão 6
+$questao6=$_POST['q6'];
+
+//questão 7
+$questao7=$_POST['q7'];
+
