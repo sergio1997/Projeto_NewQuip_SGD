6e38524df0138aa45e703df4665cb5c87fd8116a
cbrunogoncalves@github.com <buno@localhost>
2018-05-05 09:19:48 -0300

    Criado arquivo rodape Este arquivo foi para que, quando for criar o menu em uma nova funcionalidade, só precissa chamar o arquivo rodape.php

---
+++ ProjetoIntegrador3ok/visao/rodape.php
@@ -0,0 +1,16 @@
+         <!-- rodapé -->
+            <footer class="main-footer">
+                <strong>Copyright &copy; 2018 <a href="#">NewQuip</a>.</strong> Todos os direitos reservados.
+            </footer>  
+            <!-- fim controle-sidebar -->
+            <div class="control-sidebar-bg"></div>
+        </div>
+        <!--JS SCRIPTS -->
+        <!-- jQuery 2.2.0 -->
+        <script src="plugins/jQuery/jQuery-2.2.0.min.js"></script>
+        <!-- Bootstrap 3.3.5 -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="dist/js/app.min.js"></script>
+    </body>
+</html>
