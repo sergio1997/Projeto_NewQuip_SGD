dfc08d00e1feda7adcd23fc74e88f935e8cccdf0
cbrunogocalves@github.com <bruno.jua@hotmail.com>
2018-05-05 17:15:11 -0300

    Alteração do arquivo Adicionar Treinamento

--- ProjetoIntegrador3ok/visao/pagina_inicial.php
+++ ProjetoIntegrador3ok/visao/pagina_inicial.php
@@ -10,29 +10,30 @@ $exibe = @mysqli_fetch_assoc($sql);
 ?>
 <!DOCTYPE html>
 <html>
-    <?php include("cabecalho.php")?>
-        <?php include("menu.php")?>
-            <div class="content-wrapper">
-                <section class="content-header">
-                    <h1>
-                        Parte em branco
-                    </h1>
-                </section>
-                <section class="content">
-                    <div id="calendar"></div>
-                    <script type="text/javascript" src="js/vendor/jquery-1.9.1.js"></script>
-                    <script type="text/javascript" src="js/vendor/underscore-min.js"></script>
-                    <script type="text/javascript" src="js/calendar.js"></script>
-                    <script type="text/javascript">
-            var calendar = $("#calendar").calendar(
-                    {
-                        tmpl_path: "/tmpls/",
-                        events_source: function () {
-                            return [];
-                        }
-                    });
-                    </script>
+    <head>
+        <?php include("cabecalho.php") ?>
+        <?php include("menu.php") ?>
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>
+                Parte em branco
+            </h1>
+        </section>
+        <section class="content">
+            <div id="calendar"></div>
+            <script type="text/javascript" src="js/vendor/jquery-1.9.1.js"></script>
+            <script type="text/javascript" src="js/vendor/underscore-min.js"></script>
+            <script type="text/javascript" src="js/calendar.js"></script>
+            <script type="text/javascript">
+                var calendar = $("#calendar").calendar(
+                        {
+                            tmpl_path: "/tmpls/",
+                            events_source: function () {
+                                return [];
+                            }
+                        });
+            </script>
 
-                </section>
-            </div>
-   <?php include("rodape.php");
+        </section>
+    </div>
+    <?php include("rodape.php") ?>
