486ff658e104db5839246b81e7d4bfd6db513b12
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-05-14 22:37:23 -0300

    Correção de erro, Excluir grupo - não ok.

--- ProjetoIntegrador3ok/modelo/Excluirgrupo.php
+++ ProjetoIntegrador3ok/modelo/Excluirgrupo.php
@@ -1,14 +1,7 @@
 <?php
-
-
-
-		include "../modelo/conexao.php ";
-
+include "../modelo/conexao.php";
 $id = $_GET['id']; // Recebendo o valor enviado pelo link
+@mysqli_query("DELETE FROM setor WHERE id_setor='" . $id . "'"); // A instrução delete irá apagar todos os dados da id recebida
 
-@mysql_query("DELETE FROM setor WHERE id_setor='".$id."'"); // A instrução delete irá apagar todos os dados da id recebida
-
-
-
-header("Location: ../visao/ListaGrupo.php")
+header("Location: ../visao/listaGrupo.php")
 ?>
