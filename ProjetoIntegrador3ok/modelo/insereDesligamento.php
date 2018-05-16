2da7cdf4d1f763b61329e1adacabc3dfae9fc9ee
sergio1997 <sergiolavor123321@gmail.com>
2018-05-16 20:53:49 -0300

    inserirDesligamento

--- ProjetoIntegrador3ok/modelo/insereDesligamento.php
+++ ProjetoIntegrador3ok/modelo/insereDesligamento.php
@@ -1,57 +1,56 @@
-<?php 
+=<?php
 
 include "../modelo/conexao.php";
 
-$nomeFuncionario=$_POST['funcionario'];
+$nomeFuncionario = $_POST['funcionario'];
 
 
-$questao1=$_POST['funcionario'];
-$dataSaida=$_POST['dataSaida'];
-$MotivoDesligamento=$_POST['MotivoDesligamento'];
+$questao1 = $_POST['funcionario'];
+$dataSaida = $_POST['dataSaida'];
+$MotivoDesligamento = $_POST['MotivoDesligamento'];
 
 //questão 1
-$questao1=$_POST['q1'];
-$questao1PQ=$_POST['q1PQ'];
+$questao1 = $_POST['q1'];
+$questao1PQ = $_POST['q1PQ'];
 
 //questão 2
-$questao2=$_POST['q2'];
+$questao2 = $_POST['q2'];
 
 //questão 3
-$questao3=$_POST['q3'];
+$questao3 = $_POST['q3'];
 
 //questão 4
-$questao4=$_POST['q4'];
+$questao4 = $_POST['q4'];
 
-$q4Quantidade=$_POST['q4Quantidade'];
+$q4Quantidade = $_POST['q4Quantidade'];
 
 //questão 5
-$questao51=$_POST['q51'];
-$questao52=$_POST['q52'];
-$questao53=$_POST['q53'];
-$questao54=$_POST['q54'];
-$questao55=$_POST['q55'];
+$questao51 = $_POST['q51'];
+$questao52 = $_POST['q52'];
+$questao53 = $_POST['q53'];
+$questao54 = $_POST['q54'];
+$questao55 = $_POST['q55'];
 
-$q5observacao=$_POST['q5observacao'];
+$q5observacao = $_POST['q5observacao'];
 
 //questão 6
-$questao6=$_POST['q6'];
+$questao6 = $_POST['q6'];
 
 //questão 7
-$questao7=$_POST['q7'];
+$questao7 = $_POST['q7'];
 
 
-$query1=@mysql_query("SELECT * FROM funcionario WHERE Nome='$nomeFuncionario'");
-    $exibe2=@mysql_fetch_assoc($query1);
-    $id=$exibe2['id'];
-
-
-   
-
-
-$query=mysql_query("INSERT INTO desligamento (id,dataSaida,motivo,questao1,questao1PQ,questao2,questao3,questao4,q4Quantidade,questao51,questao52,questao53,questao54,questao55,q5observacao,questao6,questao7) VALUES('$id','$dataSaida','$MotivoDesligamento','$questao1','$questao1PQ','$questao2','$questao3','$questao4','$q4Quantidade','$questao51','$questao52','$questao53','$questao54','$questao55','$q5observacao','$questao6','$questao7')");
+$query1 = @mysqli_query("SELECT * FROM funcionario WHERE Nome='$nomeFuncionario'");
+$exibe2 = @mysqli_fetch_assoc($query1);
+$id = $exibe2['id'];
 
 
 
- header('location:../visao/Desligamento.php');
 
+
+$query = mysqli_query("INSERT INTO desligamento (id,dataSaida,motivo,questao1,questao1PQ,questao2,questao3,questao4,q4Quantidade,questao51,questao52,questao53,questao54,questao55,q5observacao,questao6,questao7) VALUES('$id','$dataSaida','$MotivoDesligamento','$questao1','$questao1PQ','$questao2','$questao3','$questao4','$q4Quantidade','$questao51','$questao52','$questao53','$questao54','$questao55','$q5observacao','$questao6','$questao7')");
+
+
+
+header('location:../visao/Desligamento.php');
 ?>
