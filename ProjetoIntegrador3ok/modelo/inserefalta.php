a092765ed7c4673f6ee111b48163d8b708782b16
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-05-21 17:37:00 -0300

    Alteração no arquivo inserefalta.php

--- ProjetoIntegrador3ok/modelo/inserefalta.php
+++ ProjetoIntegrador3ok/modelo/inserefalta.php
@@ -8,11 +8,11 @@ include "conexao.php";
             //função para redirecionar para outra pagina apos 5 segudos 
             //caso aultentique ou não 
             function loginsuccessfully() {
-                setTimeout("window.location='../visao/Agendar.php'");
+                setTimeout("window.location='../visao/listarfaltas.php'");
                 alert('Agenda salvo com sucesso!!');
             }
             function loginfailed() {
-                setTimeout("window.location='../visao/Agendar.php'");
+                setTimeout("window.location='../visao/agendarTarefa.php'");
                 alert('grupo que voce digitou não exite');
             }
         </script>
@@ -23,11 +23,14 @@ include "conexao.php";
         $data = $_POST['data'];
         $motivo = $_POST['motivo'];
 
-        $sqlinsere = @mysqli_query("INSERT INTO faltas(data_falta,motivo)VALUES('$data','$motivo')");
-        $sql = @mysqli_query("SELECT * FROM faltas WHERE data_falta='$data' AND motivo='$motivo'");
+        $sqlinsere = "INSERT INTO faltas(data_falta,motivo)VALUES('{$data}','{$motivo}')";
+                $sql =("SELECT * FROM faltas WHERE data_falta='{$data}' AND motivo='$motivo'");
         $exibe = @mysqli_fetch_assoc($sql);
         $id_faltas = $exibe['id_falta'];
-        $sqlinsere1 = @mysqli_query("INSERT INTO codigo_faltas(id,id_faltas)VALUES('$id_funcionario','$id_faltas')");
+        $sqlinsere1 = "INSERT INTO codigo_faltas(id,id_faltas)VALUES('${id_funcionario}','{$id_faltas}')";
+        $sql1 = mysqli_query($conexao, $sqlinsere);
+        $sql2 = mysqli_query($conexao, $sqlinsere1);
+        
         echo "<script>loginsuccessfully()</script>";
         ?>
     </body>
