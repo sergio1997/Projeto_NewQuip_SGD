65038e5965ddbb52ccbd1f88bfd0b3f0db597bb0
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-05-22 17:49:39 -0300

    Implementação model - verificaçao formulario 1

---
+++ ProjetoIntegrador3ok/modelo/verificacaoformulario1.php
@@ -0,0 +1,42 @@
+<?php
+include("../modelo/iniciarsessao.php");
+include("../modelo/conexao.php");
+
+$id = $_POST['id'];
+
+
+$query = mysqli_query("SELECT id FROM funcionario WHERE email ='$_SESSION[email]'");
+$linha = @mysqli_fetch_assoc($query);
+$sessao = $linha['id'];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <script type= "text/javascript">
+            //função para redirecionar para outra pagina apos 5 segudos 
+            //caso aultentique ou não 
+            function passa() {
+
+                setTimeout("window.location='../visao/formulario1.php?id=<?php echo $id; ?>'");
+            }
+            function volta() {
+
+                setTimeout("window.location='../visao/avaliadoformulario1.php'");
+
+                alert('você não pode se avaliar');
+            }
+        </script>
+    </head>
+    <body>
+        <?php
+        ?>
+        <?php
+        if ($id == $sessao) {
+            echo "<script>volta()</script>";
+        } else {
+            echo "<script>passa()</script>";
+        }
+        ?>
+    </body>
+</html>
