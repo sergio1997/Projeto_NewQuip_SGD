7de2b16566faf1456d5746ffdf47742397c6ab3d
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-05-29 21:28:14 -0300

    Alteração conexao

--- ProjetoIntegrador3ok/modelo/conexao.php
+++ ProjetoIntegrador3ok/modelo/conexao.php
@@ -2,7 +2,7 @@
 
 //Configura de acesso ao banco de dados
 //conexão
-$conexao = new mysqli("localhost", "root", "root", "bancodedados");
+$conexao = new mysqli("localhost", "root", "root", "bancodedados2");
 if (mysqli_connect_errno()) {
     echo "Failed connect to mysq:" . mysqli_connect_error();
 }
