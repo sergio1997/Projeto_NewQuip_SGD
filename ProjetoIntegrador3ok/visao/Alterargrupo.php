17777cec072f46c71da5e68d96025ee050f23eef
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-05-29 21:28:47 -0300

    Alteração alterargrupo

--- ProjetoIntegrador3ok/visao/Alterargrupo.php
+++ ProjetoIntegrador3ok/visao/Alterargrupo.php
@@ -3,13 +3,13 @@ include "../modelo/iniciarsessao.php";
 include "../modelo/conexao.php";
 ?>
 <?php
-$id = $_GET['id']; // Recebendo o valor enviado pelo link
+$id = $_GET['id_setor']; // Recebendo o valor enviado pelo link
 
-$sql = @mysqli_query("SELECT * FROM setor WHERE id_setor='" . $id . "'"); // A instrução delete irá apagar todos os dados da id recebida
+$sql = mysqli_query("SELECT * FROM setor WHERE id_setor='" . $id . "'"); // A instrução delete irá apagar todos os dados da id recebida
 
-$exibe = @mysqli_fetch_assoc($sql);
+$exibe = mysqli_fetch_assoc($sql);
 $id_setor = $exibe['id_setor'];
-$nome_setor = $exibe['nome_setor'];
+$nome_setor = $exibe['nm_setor'];
 ?>
 <html>
     <head>
@@ -44,7 +44,7 @@ $nome_setor = $exibe['nome_setor'];
     <?php include("menu.php") ?>
     <div class="content-wrapper">
         <section class="content-header">
-            <h1>Cadastrar Setor</h1>
+            <h1>Alterar Setor</h1>
         </section>
         <section class="content">
             <div class="box box-default color-palette-box">
@@ -55,23 +55,26 @@ $nome_setor = $exibe['nome_setor'];
                                 <div>
                                     <h3 class="box-title">Dados do Setor</h3>
                                 </div>
-                                <div class="box-body">
-                                    <div class="panel box box-primary">             
-                                    </div>
-                                    <form action="../modelo/salvarAlteracaoGrupo.php" method="post" name="f1" onSubmit="return valida_dados(this)">
-                                        <div class="form-group has-feedback">
-                                            <b>Código<font color="#FF0000" >*</font></b> 
-                                            <input type="text"  class="form-control" name="id_setor" value="<?php echo"$id"; ?>" maxlength="40" placeholder="ex: Finaceiro" onKeypress='return soLetras(event)' />
-                                        </div>
-                                        <div class="form-group has-feedback">
-                                            <b>Nome do Setor<font color="#FF0000" >*</font></b> 
-                                            <input type="text"  class="form-control" name="nome_setor" value="<?php echo "$nome_setor" ?>" maxlength="40" placeholder="ex: Finaceiro" onKeypress='return soLetras(event)' />
-                                        </div>
-                                        <div>
-                                            <input type="submit" class="btn btn-primary" value="Cadastrar" />
-                                        </div>  
-                                    </form>
-                                </div>
-                                </section>
                             </div>
-<?php include ("rodape.php")?>
+                        </div>
+                    </div>
+                    <div class="box-body">
+                        <div class="panel box box-primary">             
+                        </div>
+                        <form action="../modelo/salvarAlteracaoGrupo.php" method="post" name="f1" onSubmit="return valida_dados(this)">
+                            <div class="form-group has-feedback">
+                                <b>Código<font color="#FF0000" >*</font></b> 
+                                <input type="text"  class="form-control" name="id_setor" value="<?php echo"$id"; ?>" maxlength="40" placeholder="ex: Finaceiro" onKeypress='return soLetras(event)' />
+                            </div>
+                            <div class="form-group has-feedback">
+                                <b>Nome do Setor<font color="#FF0000" >*</font></b> 
+                                <input type="text"  class="form-control" name="nm_setor" value="<?php echo "$nome_setor" ?>" maxlength="40" placeholder="ex: Finaceiro" onKeypress='return soLetras(event)' />
+                            </div>
+                            <div>
+                                <input type="submit" class="btn btn-primary" value="Cadastrar" />
+                            </div>  
+                        </form>
+                    </div>
+                </section>
+            </div>
+            <?php include ("rodape.php") ?>
