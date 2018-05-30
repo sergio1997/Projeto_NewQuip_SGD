cb96c7b610e7d8d2ad3b0be49c939f8b2263df50
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-05-29 21:32:07 -0300

    Alteração insereGrupo

--- ProjetoIntegrador3ok/modelo/insereGrupo.php
+++ ProjetoIntegrador3ok/modelo/insereGrupo.php
@@ -5,7 +5,7 @@ include "../modelo/conexao.php";
 $nomeSetor =filter_input(INPUT_POST, 'nomeSetor', FILTER_SANITIZE_STRING);
 // echo "nomeSetor: $nomeSetor";
 //$nomeSetor = $_POS['nomeSetor'];
-$sqlInsere = "INSERT INTO setor (nome_setor) VALUES ('$nomeSetor')";
+$sqlInsere = "INSERT INTO setor (nm_setor) VALUES ('$nomeSetor')";
 mysqli_query($conexao, $sqlInsere);
 
 //INSERT INTO ClientesCidades (NomeCliente, CidadeCliente, CredCliente) SELECT cc_nome, cc_cidade, cc_cred FROM CliCred WHERE cc_cred > 300;
