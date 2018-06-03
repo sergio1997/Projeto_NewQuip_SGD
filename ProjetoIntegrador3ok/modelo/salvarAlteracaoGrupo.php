d18e359b4f82f8f201c5c34f8d10bb4a9391ae9e
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-06-03 17:54:31 -0300

    Alteração arquivo SalvarAlteração Grupo

--- ProjetoIntegrador3ok/modelo/salvarAlteracaoGrupo.php
+++ ProjetoIntegrador3ok/modelo/salvarAlteracaoGrupo.php
@@ -1,15 +1,15 @@
+
 <?php 
 include ("../modelo/conexao.php"); 
 include ("../modelo/iniciarsessao.php"); 
-
- $id_setor= $_GET['id'];
+ 
+  $id_setor= $_GET['id'];
   $nm_setor= $_GET['nm_setor'];
+  
+//alterando dados no banco
 
-//Altera dados no banco
 $query=mysqli_query($conexao,"UPDATE  setor SET nm_setor='$nm_setor' WHERE id_setor='".$id_setor."'"); // A instrução delete irá apagar todos os dados da id recebida
+ //echo"alert('Dados de acesso alterado com sucesso!!');";
 
- echo"alert('Dados alterado com sucesso!!');";
-?>
-<?PHP
   header('location:../visao/listaGrupo.php');
 ?>
