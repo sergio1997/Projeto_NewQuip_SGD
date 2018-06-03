b69294a0b7367b812f4914218a24ad2e023ffbac
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-06-03 20:00:07 -0300

    Correção de erro AdicionarTreinamento

--- ProjetoIntegrador3ok/modelo/AdicionaTreinamento.php
+++ ProjetoIntegrador3ok/modelo/AdicionaTreinamento.php
@@ -4,35 +4,35 @@ include "conexao.php";
 <html>
     <head>
         <title>Rsultado</title>
-        <script type= "text/javascript">
+          <script type= "text/javascript">
             //função para redirecionar para outra pagina apos 5 segudos 
             //caso aultentique ou não 
             function loginsuccessfully() {
-                setTimeout("window.location='../visao/Agendar.php'");
-                alert('Agenda salvo com sucesso!!');
+                setTimeout("window.location='../visao/adicionarTreinamento.php'");
+                alert(' Treinamento salvo com sucesso!!');
             }
             function loginfailed() {
-                setTimeout("window.location='../visao/Agendar.php'");
+                setTimeout("window.location='../visao/adicionarTreinamento.php'");
 
-                alert('grupo que voce digitou não exite');
+                alert(' Usuário inesistente!');
             }
         </script>
     </head>
-    <body>
+   <body>
         <?php
         $funcionario = $_POST['nome'];
         $evolucao = $_POST['evolucao'];
         $desempenho = $_POST['desempenho'];
         $observacoes = $_POST['observacoes'];
 
-        $sql = @mysqli_query("SELECT * FROM funcionario WHERE Nome LIKE '%" . $funcionario . "%'");
-        $row = @mysqli_num_rows($sql);
+        $sql = mysqli_query( $conexao,"SELECT * FROM nome_usuario WHERE nome LIKE '%" . $funcionario . "%'");
+        $row = mysqli_num_rows($sql);
         if ($row > 0) {
             while ($linha = mysqli_fetch_array($sql)) {
-                $nome = $linha['Nome'];
-                echo "$nome";
+                $nome = $linha['nome'];
+                
             }
-            $sqlinsere = @mysqli_query("INSERT INTO treinamento(nome,evolucao,desempenho,observacoes)VALUES('$nome','$evolucao','$desempenho','$observacoes')");
+            $sqlinsere = @mysqli_query($conexao,"INSERT INTO treinamento (nome,evolucao,desempenho,observacao)VALUES('$nome','$evolucao','$desempenho','$observacoes')");
             echo "<script>loginsuccessfully()</script>";
         } else {
             echo "<script>loginfailed()</script>";
