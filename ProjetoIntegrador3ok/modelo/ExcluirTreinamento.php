d08a653482e5c01e854b6a4cc887eff95f6227f0
cbrunogoncalves@github.com <bruno.jua@hotmail.com>
2018-06-03 20:01:33 -0300

    Correção de erro ExcluirTreinamento

--- ProjetoIntegrador3ok/modelo/ExcluirTreinamento.php
+++ ProjetoIntegrador3ok/modelo/ExcluirTreinamento.php
@@ -1,6 +1,6 @@
 <?php
 include "../modelo/conexao.php";
 $id = $_GET['id']; // Recebendo o valor enviado pelo link
-@mysqli_query("DELETE FROM treinamento WHERE id_treinamento='" . $id . "'"); // A instrução delete irá apagar todos os dados da id recebida
+@mysqli_query($conexao,"DELETE FROM treinamento WHERE id_treinamento='" . $id . "'"); // A instrução delete irá apagar todos os dados da id recebida
 header("Location: ../visao/listarTreinamento.php")
 ?>
